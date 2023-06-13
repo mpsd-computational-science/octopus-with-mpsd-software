9a2d5776e45eb2c746be0036e5837ed3eae09e5b
iamashwin99 <ashwin-kumar.karnad@mpsd.mpg.de>
2023-06-13 14:46:38 +0200

    add octopus example

--- install-octopus.sh
+++ install-octopus.sh
@@ -6,6 +6,9 @@ eval `/usr/share/lmod/lmod/libexec/lmod load toolchains/$TOOLCHAIN`
 source $TOOLCHAIN-config.sh --prefix=`pwd`
 make -j
 make install
+# run a simple octopus example
+echo "CalculationMode = reci" > inp
+bin/octopus
 echo "make check is next"
 
 
